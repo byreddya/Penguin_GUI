4c9a6a34dae9aa3ba898f70858eb091c73141045
byreddya <byreddy.a@turing.com>
2025-03-14 11:10:20 +0530

    modified

--- PRBS/Design/PRBS.v
+++ PRBS/Design/PRBS.v
@@ -1,23 +1,24 @@
 module prng(
     input clk,
     input reset,
-    output reg random_number
+    input [6:0] seed,
+    output reg [7:0] random_number
 );
 
-    parameter SEED = 7'd1; // Initial seed
-    parameter POLYNOMIAL = 7'b1100000; // 6th-degree polynomial: x^6 + x^5 + x^3 + x^2 + 1
+    parameter POLYNOMIAL = 7'b1100000; // 6th-degree polynomial: x^6 + x^5 + 1
 
-  reg [6:0] state;
+    reg [6:0] state;
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            state <= SEED;
+            state <= seed;
+            random_number <= 8'd0;
         end else begin
             // Calculate the LFSR output
-          random_number <= state[6]; // The least significant bit is the random output
+            random_number <= {state[6],state}; // The least significant bit is the random output
 
             // Shift the state left and apply the feedback polynomial
-          state <= {state[5:0], (state[6] ^ state[5])};
+            state <= {state[5:0], (state[6] ^ state[5])};
         end
     end
 
